3f8afb6e18684d35e931b5113f379d73fd13b3b4
Dara Tumenbayeva <tumenbayevadara@gmail.com>
2018-10-14 01:10:30 +0800

    enhancing topics

--- calculating-percentage.py
+++ calculating-percentage.py
@@ -5,20 +5,27 @@ final_data = []
 my_list = []
 rm = ",:;?/-!."
 topics_set = set()
-number_set = set()
 with codecs.open(os.path.abspath("/Users/dara/Desktop/Projects/python-extractor/new-dataset.csv")) as f:
     for line in f:
         a = line.split(',')
         for t in a:
             # print(t)
-            topics_data.append(t)
+            if t == 'reply':
+                t = 'no reply'
+            if t == 'network' or t == 'slow':
+                t = 'slow network'
+            if t == 'sembilan' or t == 'negeri':
+                t = 'sembilan negeri'
+            if t == 'worst' or t == 'telco':
+                t = 'worst telco'
+            topics_data.append(t.rstrip())
 
 for i in topics_data:
-    # print(i)
     if i not in my_list:
-        print([i, topics_data.count(i)])
-        final_data.append([i, topics_data.count(i)])
-        my_list.append(i)
+        if i != 'celcom':
+            print([i, topics_data.count(i)])
+            final_data.append([i, topics_data.count(i)])
+            my_list.append(i)
     # number_set.add(topics_data.count(i))
 # print(final_data)
 # print  # (number_set)
